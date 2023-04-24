ec4b7abd429fb9022f78964ef49fb95ff8b5987a
Peter Woods <woodspeter37@gmail.com>
2023-04-25 00:33:02 +0100

    JUnit Tests for adding to the collection

---
+++ src/test/kotlin/controllers/PlayerAPITest.kt
@@ -0,0 +1,66 @@
+package controllers
+
+import models.Player
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class PlayerAPITest {
+
+    private var peter: Player? = null
+    private var james: Player? = null
+    private var joe: Player? = null
+    private var bob: Player? = null
+    private var mary: Player? = null
+    private var populatedPlayers: PlayerAPI? = PlayerAPI()
+    private var emptyPlayers: PlayerAPI? = PlayerAPI()
+
+    @BeforeEach
+    fun setup() {
+        peter = Player("Peter Woods", "11-09-2002", 9, false)
+        james = Player("James Power", "13-02-2000", 8, false)
+        joe = Player("Joe Doe", "02-04-2003", 7, false)
+        bob = Player("Bob Builder", "22-10-1999", 6, false)
+        mary = Player("Mary Daly", "30-09-1998", 7, false)
+
+        //adding 5 Note to the players api
+        populatedPlayers!!.add(peter!!)
+        populatedPlayers!!.add(james!!)
+        populatedPlayers!!.add(joe!!)
+        populatedPlayers!!.add(bob!!)
+        populatedPlayers!!.add(mary!!)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        peter = null
+        james = null
+        joe = null
+        bob = null
+        mary = null
+        populatedPlayers = null
+        emptyPlayers = null
+    }
+
+    @Test
+    fun `adding a Player to a populated list adds to ArrayList`() {
+        val newPlayer = Player("Leo Messi", "10-08-1993", 10, false)
+        assertEquals(5, populatedPlayers!!.numberOfPlayers())
+        assertTrue(populatedPlayers!!.add(newPlayer))
+        assertEquals(6, populatedPlayers!!.numberOfPlayers())
+        assertEquals(newPlayer, populatedPlayers!!.findPlayer(populatedPlayers!!.numberOfPlayers() - 1))
+    }
+
+    @Test
+    fun `adding a Player to an empty list adds to ArrayList`() {
+        val newPlayer = Player("Leo Messi", "10-08-1993", 10, false)
+        assertEquals(0, emptyPlayers!!.numberOfPlayers())
+        assertTrue(emptyPlayers!!.add(newPlayer))
+        assertEquals(1, emptyPlayers!!.numberOfPlayers())
+        assertEquals(newPlayer, emptyPlayers!!.findPlayer(emptyPlayers!!.numberOfPlayers() - 1))
+    }
+}
+
+
