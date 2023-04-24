872275d9ea3f1cd7d3b22f34cecf0cf24140f1ac
Peter Woods <woodspeter37@gmail.com>
2023-04-24 23:51:46 +0100

    Skeleton code for Team app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,70 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.System.exit
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+fun main(args: Array<String>) {
+    runMenu()
 }
+
+    val scanner = Scanner(System.`in`)
+
+
+
+
+fun mainMenu() : Int {
+    print("""
+        >  ----------------------------------
+        >  |        NOTE KEEPER APP         |
+        >  ----------------------------------
+        > | NOTE MENU                      |
+        >  |   1) Add a player              |
+        >  |   2) List all players          |
+        >  |   3) Update a player           |
+        >  |   4) Delete a player           |
+        >  ----------------------------------
+        >  |   0) Exit                      |
+        >  ----------------------------------
+        >  ==>> """)
+    return scanner.nextInt()
+}
+
+
+
+fun runMenu() {
+        do {
+            val option = mainMenu()
+            when (option) {
+                1  -> addPlayer()
+                2  -> listPlayers()
+                3  -> updatePlayer()
+                4  -> deletePlayer()
+                0  -> exitApp()
+                else -> println("Invalid option entered: ${option}")
+
+            }
+        } while (true)
+    }
+
+    fun addPlayer(){
+        println("You chose Add Player")
+    }
+
+    fun listPlayers(){
+        println("You chose List Players")
+    }
+
+    fun updatePlayer(){
+        println("You chose Update Player")
+    }
+
+    fun deletePlayer(){
+        println("You chose Delete Player")
+    }
+
+    fun exitApp(){
+        println("Exiting...bye")
+        exit(0)
+    }
+
+
+
